bca6a4399b105e9721e969fc5842ccd1461a17b0
akhil683 <onkar.palsra@gmail.com>
2025-09-13 22:36:13 +0530

    fix: small bugs in UI

--- components/bar/theme-toggle.tsx
+++ components/bar/theme-toggle.tsx
@@ -4,8 +4,10 @@ import { useTheme } from "../providers/ThemeProvider";
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className="w-full flex justify-between">
-      <span className="text-gray-950 dark:text-gray-100">Dark Mode</span>
+    <div className="w-full flex justify-between md:justify-center lg:justify-between">
+      <span className="md:hidden lg:flex text-sm text-gray-950 dark:text-gray-100">
+        Dark Mode
+      </span>
       <Switch
         checked={theme === "dark"}
         onCheckedChange={toggleTheme}
